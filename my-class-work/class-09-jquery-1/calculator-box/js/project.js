87b349d760bc1eef4ff3bb585406652504fe085b
ellamiowgi <ladonne@gmail.com>
2015-12-01 18:31:40 -0500

    temp calc homework & calculator box homework

--- my-class-work/class-09-jquery-1/calculator-box/js/project.js
+++ my-class-work/class-09-jquery-1/calculator-box/js/project.js
@@ -13,9 +13,31 @@ $(document).ready(function () {
 		$('#out').text(total)
 	}
 
+	$('#a20').click(add20)
+
+	function add20 () {
+		total = total + 20
+		$('#out').text(total)
+	}
 
 
+	// Optimum solution for numbers
+	// Love, Jon
+	var $nums = $('.num');
+	var $out = $('#out');
 
+	// $nums.click(function(e) {
+	// 	var num = $(this).data().num;
+	// 	total += num;
+	// 	$out.text(total);
+	// })
+
+	var $colors = $('.color');
+
+	// $colors.click(function(e) {
+	// 	var color = $(this).data().color;
+	// 	$out.css('background-color', color);
+	// })
 
 
 
